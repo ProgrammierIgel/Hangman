322815781a7ebc49a8501aa65fb8700a242a6da5
ProgrammierIgel <erwinmueller968@gmail.com>
2023-10-05 16:41:30 +0200

    inital commit

---
+++ Add.py
@@ -0,0 +1,85 @@
+
+class Add:
+    def __init__(self, main):
+        self.words = []
+        self.read()
+        self.main = main
+
+    def add(self, word):
+        with open("./words.txt", "a") as file:
+
+            file.write("{}\n".format(word.replace(" ","")))
+            file.close()
+        self.read()
+
+    def read(self):
+        self.words = []
+        with open("./words.txt", "r") as file:
+
+            for line in file.readlines():
+                word = line.replace("\n", "").upper()
+                self.words.append(word.replace("Ã¶", "Ü").replace("Ã„", "Ä").replace("ÃŒ", "Ü").replace("Ã–", "Ö"))
+                file.close()
+
+    def user_adds(self):
+        run = True
+        while run:
+            entry = input("Bitte gib ein neues Wort ein! ZU Abrechen gib bitte '/exit' ein! Für Hilfe: '/help' \n >>>")
+            if entry == "/exit":
+                run = False
+            elif entry == "/help":
+                self.main.print_help()
+            elif entry in self.words:
+                print("wort schon vorhanden!")
+            else:
+                self.add(str(entry))
+                print(self.words)
+
+    def edit(self):
+        while True:
+            for i in range(len(self.words)):
+                print("{}: {}".format(i+1, self.words[i]))
+            entry = input("Bitte gib die Nummer des Wortes ein, welches du bearbeiten möchtest! Um zu beenden :'/exit'\n")
+            if entry.startswith("/exit"):
+                break
+            if entry.startswith("/help"):
+                self.main.print_help()
+                continue
+            if entry == "\n":
+                continue
+            try:
+                chois = int(entry)-1
+            except:
+                print("Ungültige Eingabe!")
+                print()
+                continue
+            print()
+            print("{}".format(self.words[chois]))
+
+            new = input("Bitte gib das korigierte Wort jetzt ein or '/delete' zum löschen!\n>")
+            if not new.startswith("/delete"):
+                new = new.upper()
+                print(new)
+                yn = input("Bist du dir sicher? [y]/n :")
+            else:
+                yn = input("Bist du dir sicher. dass du {} löschen möchtest? [y]/n :".format(self.words[chois]))
+            if yn == "":
+                if new.startswith("/delete"):
+                    self.words.pop(chois)
+                    self.update()
+                else:
+                    self.words[chois] = new
+                    self.update()
+                print("Änderung wurde erfolgreich gespeichert")
+            if yn.lower() == "y" or yn.lower() == "yes" or yn.lower() == "ja" :
+                self.words[chois] = new
+                self.update()
+                print("Änderung wurde erfolgreich gespeichert")
+                continue
+            else:
+                continue
+
+    def update(self):
+        with open("./words.txt", "w") as file:
+            for i in self.words:
+                file.write("{}\n".format(i.replace(" ", "")))
---
+++ main.py
@@ -0,0 +1,197 @@
+import json
+import random
+import Add
+import Fehler
+
+
+class Hangman:
+    def __init__(self):
+        self.Add = Add.Add(self)
+        self.words = self.Add.words
+        self.highscore = json.loads(open("./highscore.json").read())['highscore']
+
+
+
+        self.waiting_for_command()
+
+    def print_help(self):
+        print("""HILFE FÜR USER:
+         - um zu beenden: 'exit'
+         - für die Hilfe: 'help'
+         - um Wörter hinzuzufügen: 'add'
+         - um zu spielen: 'play'
+         - Wörter bearbeiten: 'edit'
+         
+         PLAY: Man hat 11 Versuche. Bitte gib das Wort oder einzelne Buchstaben ein. Keine Wortgruppe!
+         Eingaben werden in Großbuch umgewandelt!
+         um den Spielmodus zu beenden :'/exit'
+         
+         ADD: Modus um Wörter hinzuzufügen.
+         Wörter werden automatisch in Wörter, verwandelt bei denen alle Buchstaben groß geschrieben sind!
+         Hinzufügenmodus beenden mit: '/exit'.
+         
+         EDIT: Gib die Nummer des Wortes ein welches du bearbeiten möchtest.
+         Die Wörter, welche zur Auwahl stehen werden dir angezeigt.
+         Gib nun das korigierte Wort ein. Bestätige nun deine Eingabe mit einem Ja!
+         Um den Bearbeitungsmodus zu verlassen gib bitte '/exit' ein!. 
+         
+         OVERVIEW: Zeigt alle vorhnadenen Wörter. 
+         
+         EXIT: Beendet das Programm.  
+         
+         HELP: Zeigt die Hilfe an.
+              
+         """)
+
+    def waiting_for_command(self):
+        while True:
+            print("Für die Hilfe: 'help'")
+            print("Zum beenden: 'exit'")
+            entry = input("Bitte gib einen Befehl ein! \n >>> ")
+
+            if entry == "play":
+                self.play()
+            elif entry == "add":
+                self.clear_terminal()
+                self.Add.user_adds()
+            elif entry == "help":
+                self.print_help()
+            elif entry == "exit":
+                exit()
+            elif entry == "\n" or entry == "":
+                continue
+            elif entry== "edit":
+                self.Add.edit()
+            elif entry == "overview":
+                for i in range(len(self.words)):
+                    print("{}. {}".format(i+1, self.words[i]))
+            else:
+                print("FEHLER: Eingabe ungültig!\n Für die Hilfe gib bitte 'help' ein !")
+
+    def play(self):
+        run= True
+        score = 0
+        while run:
+            wordnumber = random.randint(0, len(self.words)-1)
+            word = self.words[wordnumber]
+            geraten = []
+            Fehler = 0
+            self.clear_terminal()
+            while True:
+
+                unknown= 0
+                for letter in word:
+                    if letter in geraten:
+                        print(letter, end="")
+                    else:
+                        print("_ ", end="")
+                        unknown+=1
+                if unknown ==0:
+                    print()
+                    print()
+                    print("GEWONNEN")
+                    score +=1
+                    if score >= self.highscore:
+                        self.highscore = score
+                        with open('./highscore.json') as f:
+                            data = json.load(f)
+                        data["highscore"] = self.highscore
+                        with open('./highscore.json', 'w') as outfile:
+                            json.dump(data, outfile)
+                        print("SUPER! Du hast deinen Highscore geknackt! Highscore: {}".format(self.highscore))
+                    break
+
+                print()
+                entry = input("Bitte gib einen Buchstaben oder das Wort ein. Zu Abbrechen '/exit'! Hilfe: '/help'  Score: '/score' Highscore: '/highscore' \n").upper()
+                if entry.startswith("/EXIT"):
+                    self.clear_terminal()
+                    run = False
+                    break
+                elif entry.startswith("/HELP"):
+                    self.print_help()
+                    continue
+                elif entry == "\n":
+                    continue
+                elif entry.startswith("/SCORE"):
+                    print("DU hast gerade {} Punkte.".format(score))
+                    continue
+
+                elif entry.startswith("/HIGHSCORE"):
+                    print("Dein Highscore beträgt gerade {} Punkte!".format(self.highscore))
+                    continue
+
+                elif entry == word:
+                    print("Prima! Du hast gewonnen!")
+                    print("Das Wort war: {}".format(word))
+                    score =score+1
+                    if score >= self.highscore:
+                        self.highscore = score
+                        with open('./highscore.json') as f:
+                            data = json.load(f)
+                        data["highscore"] = self.highscore
+                        with open('./highscore.json', 'w') as outfile:
+                            json.dump(data, outfile)
+
+                        print("SUPER! Du hast deinen Highscore geknackt! Highscore: {}".format(self.highscore))
+
+                    break
+                elif len(entry)>1:
+                    print("Bitte nur Buchstaben oder das ganze Wort!")
+                    continue
+                elif entry in geraten:
+                    print("Eingabe schon vorhanden!")
+                    continue
+                geraten.append(entry)
+                if entry in word:
+                    print("super!")
+                else:
+                    print("Leider Falsch!")
+                    Fehler+=1
+                    self.print_misstake(Fehler)
+                    if Fehler >= 11:
+                        print("GAME OVER")
+                        print("Du wurdest gehängt")
+                        print("Das Wort war: {}".format(word))
+                        break
+
+
+
+    def print_misstake(self, misstake):
+        if misstake == 1:
+            print(Fehler.eins)
+        elif misstake ==2:
+            print(Fehler.zwei)
+        elif misstake == 3:
+            print(Fehler.drei)
+        elif misstake == 4:
+            print(Fehler.vier)
+        elif misstake == 5:
+            print(Fehler.fünf)
+        elif misstake == 6:
+            print(Fehler.sechs)
+        elif misstake == 7:
+            print(Fehler.sieben)
+        elif misstake == 8:
+            print(Fehler.acht)
+        elif misstake == 9:
+            print(Fehler.neun)
+        elif misstake == 10:
+            print(Fehler.zehn)
+        elif misstake == 11:
+            print(Fehler.elf)
+        elif misstake == 0:
+            print("""
+            
+            
+            
+            
+            """)
+
+    def clear_terminal(self):
+        for x in range(9):
+            print()
+
+
+
+if __name__ == "__main__":
+    Hangman()
